907346491803244e26f173cc6d7fd61f39b03d53
Mike <mjmurray4@gmail.com>
2020-04-10 18:40:20 -0700

    Complete 260

---
+++ Todoey/DataModel/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Michael Murray on 4/10/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
